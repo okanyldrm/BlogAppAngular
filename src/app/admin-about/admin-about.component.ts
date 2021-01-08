f3d50803228fb808795926256de2900fbc1a6c32
okanyldrm <okanyldrm68@gmail.com>
2021-01-08 22:47:53 +0300

    aboutpage service and ui done

--- src/app/admin-about/admin-about.component.ts
+++ src/app/admin-about/admin-about.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AboutPage } from '../models/AboutPage';
+import { AboutpageService } from '../services/aboutpage.service';
+import { Sweetalert2Service } from '../services/sweetalert2.service';
 
 @Component({
   selector: 'app-admin-about',
   templateUrl: './admin-about.component.html',
-  styleUrls: ['./admin-about.component.css']
+  styleUrls: ['./admin-about.component.css'],
 })
 export class AdminAboutComponent implements OnInit {
-
-  constructor() { }
+  aboutpage: AboutPage = new AboutPage();
+  constructor(
+    private aboutpageService: AboutpageService,
+    private sweetalet: Sweetalert2Service
+  ) {}
 
   ngOnInit(): void {
+    this.aboutpageService
+      .getaboutpage()
+      .subscribe((data) => (this.aboutpage = data));
   }
 
+  updateform(form:NgForm){
+this.aboutpageService.updateaboutpage(this.aboutpage).subscribe((data)=>{
+  this.sweetalet.fire("Güncelleme Başarılı");
+})
+  }
 }
---
+++ src/app/models/AboutPage.ts
@@ -0,0 +1,9 @@
+export class AboutPage{
+    id!:number;
+    title!:string;
+    miniTitle!:string;
+    content!:string;
+    title2!:string;
+    content2!:string;
+    imageUrl!:string
+}
